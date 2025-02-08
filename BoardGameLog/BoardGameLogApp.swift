8a7f2a6565e50942ced9568c1a3b82782342e36f
ivan-o-github <127018501+ivan-o-github@users.noreply.github.com>
2025-02-08 22:10:28 +0200

    Initial Commit

---
+++ BoardGameLog/BoardGameLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameLogApp.swift
+//  BoardGameLog
+//
+//  Created by Ivan Osipov on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardGameLogTests/BoardGameLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameLogTests.swift
+//  BoardGameLogTests
+//
+//  Created by Ivan Osipov on 08.02.2025.
+//
+
+import Testing
+@testable import BoardGameLog
+
+struct BoardGameLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
